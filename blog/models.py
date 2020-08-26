bec60fed1990b870fa3e75e7ebd2c2ef7c0d4396
Ashish Ranjan <ashish@tug.ro>
2020-08-27 01:39:14 +0530

    feat(blog): Add post_list and post_detail urls

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -32,3 +33,11 @@ class Post(models.Model):
     # redefined explicitly if another manager is also defined
     objects = models.Manager()
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug,
+        ])
