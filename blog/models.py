2750ee03a37d26e0df3ad0dc325178a566d5267d
Ashish Ranjan <ashish@tug.ro>
2020-08-27 00:21:20 +0530

    feat(blog): Add published model manager

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -23,3 +27,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # The first manager is the default manager, so objects need to be
+    # redefined explicitly if another manager is also defined
+    objects = models.Manager()
+    published = PublishedManager()
