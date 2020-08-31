b87482478bfb86d51f8784b8bcb4dce6af896d4b
Ashish Ranjan <ashish@tug.ro>
2020-09-01 00:54:29 +0530

    feat(fts): Stemming and Ranking results

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from blog.models import Post
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -93,8 +93,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body')
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
     return render(request, 'blog/post/search.html',
         {'form': form, 'query': query, 'results': results})
