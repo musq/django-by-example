78f89d4f55788bf09879096d00c7a87323868307
Ashish Ranjan <ashish@tug.ro>
2020-09-01 01:09:57 +0530

    feat(fts): Search with trigram similarity

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,9 @@ from blog.models import Post
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector, SearchQuery, SearchRank, TrigramSimilarity
+)
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -93,11 +95,14 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', weight='A') + \
-                SearchVector('body', weight='B')
-            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                rank=SearchRank(search_vector, search_query)
-            ).filter(rank__gte=0.3).order_by('-rank')
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+            # search_vector = SearchVector('title', weight='A') + \
+            #     SearchVector('body', weight='B')
+            # search_query = SearchQuery(query)
+            # results = Post.published.annotate(
+            #     rank=SearchRank(search_vector, search_query)
+            # ).filter(rank__gte=0.3).order_by('-rank')
     return render(request, 'blog/post/search.html',
         {'form': form, 'query': query, 'results': results})
