6529f853786cdbc75df6f88e9b105616472051f0
Ashish Ranjan <ashish@tug.ro>
2020-09-01 00:22:41 +0530

    feat(blog): Add fulltext search using SearchVector

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from blog.models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -83,3 +84,17 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blog/post/share.html',
                   {'post': post, 'form': form, 'sent': sent})
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body')
+            ).filter(search=query)
+    return render(request, 'blog/post/search.html',
+        {'form': form, 'query': query, 'results': results})
