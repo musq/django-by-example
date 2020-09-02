9ab1049cc636aae82235d19d2db122e4157080a5
Ashish Ranjan <ashish@tug.ro>
2020-09-03 00:26:40 +0530

    feat(bm): Add notification using messages framework

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -6,6 +6,7 @@ from .forms import (
     LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 )
 from .models import Profile
+from django.contrib import messages
 
 def user_login(request):
     if request.method == 'POST':
@@ -59,6 +60,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
