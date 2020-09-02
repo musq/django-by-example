ae136a563e693cee79bb7bca26c4c9f2ae99e346
Ashish Ranjan <ashish@tug.ro>
2020-09-02 19:16:42 +0530

    feat(bm): Add signup views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -30,4 +30,6 @@ urlpatterns = [
     path('reset/done/',
          auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
+
+    path('register/', views.register, name='register'),
 ]
