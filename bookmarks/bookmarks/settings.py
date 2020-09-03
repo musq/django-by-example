c21945e6347e954be34a556c969e9de243d2a47f
Ashish Ranjan <ashish@tug.ro>
2020-09-03 17:46:35 +0530

    feat(bm): Install social-auth-app-django for social authentication

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j$jo=uv+c_p+kya3!nmy^=uk!d!kwmh0$7!)n@+o&2r@$v!=3h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
 ]
 
 MIDDLEWARE = [
