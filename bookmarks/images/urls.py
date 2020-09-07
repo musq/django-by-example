85e5cfc670ae3cbacd58a2969d2dd8187ed4d07b
Ashish Ranjan <ashish@tug.ro>
2020-09-07 11:19:49 +0530

    feat(bm): Add image ranking using Redis ordered sets

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
          name='detail'),
     path('like/', views.image_like, name='like'),
     path('', views.image_list, name='list'),
+    path('ranking/', views.image_ranking, name='ranking'),
 ]
