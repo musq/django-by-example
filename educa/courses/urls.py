8d398fc21edcfa68e7c05f513aa3cf9f2de03cd6
Ashish Ranjan <ashish@tug.ro>
2020-09-19 18:00:06 +0530

    feat(educa): Use formset to add multiple modules to a course

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
          name='course_edit'),
     path('<pk>/delete/', views.CourseDeleteView.as_view(),
          name='course_delete'),
+    path('<pk>/module/', views.CourseModuleUpdateView.as_view(),
+         name='course_module_update'),
 ]
