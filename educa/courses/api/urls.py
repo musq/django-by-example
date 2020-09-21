56e75dfbc485111ad84ef9cdaf663b390e03b461
Ashish Ranjan <ashish@tug.ro>
2020-09-21 22:37:40 +0530

    feat(drf): Use action in viewset to enroll students (custom route)

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -16,9 +16,5 @@ urlpatterns = [
          views.SubjectDetailView.as_view(),
          name='subject_detail'),
 
-    path('courses/<pk>/enroll/',
-         views.CourseEnrollView.as_view(),
-         name='course_enroll'),
-
     path('', include(router.urls)),
 ]
